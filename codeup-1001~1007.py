b04a73f7496adeebcc3afc36b9f21e6b19ed8a18
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-07-28 21:24:50 +0900

    Add files via upload

---
+++ codeup-1001~1007.py
@@ -0,0 +1,20 @@
+#1001
+print("Hello")
+
+#1002
+print("Hello World")
+
+#1003
+print("Hello\nWorld")
+
+#1004
+print("'Hello'")
+
+#1005 큰 따옴표 출력하기
+print("\"Hello World\"")
+
+#1006
+print("\"!@#$%^&*()\"")
+
+#1007
+print("\"C:\\Download\\hello.cpp\"")
