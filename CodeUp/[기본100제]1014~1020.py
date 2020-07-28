7f035ecbcb23a55f9dc8850790b7752673823754
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-07-29 01:46:22 +0900

    Add files via upload

---
+++ CodeUp/[기본100제]1014~1020.py
@@ -0,0 +1,23 @@
+#1014
+a,b=map(str,input().split())
+print("%c %c"%(b,a))
+
+#1015
+a=float(input())
+print("%.2f"%a)
+
+#1017
+a=int(input())
+print("%d %d %d"%(a,a,a))
+
+#1018
+h,m=map(int,input().split(':'))
+print("%d:%d"%(h,m))
+
+#1019
+y,m,d=map(int,input().split('.'))
+print("%04d.%02d.%02d"%(y,m,d))
+
+#1020
+f,l=map(str,input().split('-'))
+print("%s%s"%(f,l))
