64ca26617cd2f62dc80f170a1ff20a6c8b5140f7
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-08-04 00:12:10 +0900

    upload file

---
+++ CodeUp/[기본100제]1051~1058.py
@@ -0,0 +1,32 @@
+#1051
+a,b=map(int,input().split())
+print("%d"%(a<=b))
+
+#1052
+a,b=map(int,input().split())
+print("%d"%(a!=b))
+
+#1053
+a=bool(int(input()))
+print("%d"%(not a))
+
+#1054
+a,b=map(bool,map(int,input().split()))
+print("%d"%(a&b))
+
+#1055
+a,b=map(bool,map(int,input().split()))
+print("%d"%(a or b))
+
+#1056
+a,b=map(bool,map(int,input().split()))
+print("%d"%(a !=b))
+
+#1057
+a,b=map(bool,map(int,input().split()))
+print("%d"%(a ==b))
+
+#1058
+a,b=map(bool,map(int,input().split()))
+print("%d"%(a ==0 and b==0))
+
