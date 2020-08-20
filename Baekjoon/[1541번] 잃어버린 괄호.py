29e98f54b59994dd198f11f31c89c79029d3a4f7
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-08-21 02:16:17 +0900

    upload file

--- Baekjoon/[1541번] 잃어버린 괄호.py
+++ Baekjoon/[1541번] 잃어버린 괄호.py
@@ -1,17 +1,33 @@
 #그리디 알고리즘
 #백준 1541번
 
-a=list(input())
+a=input()
+eq=list()
+tmp=""
 minus=list()
-plus=list()
-summinus=0
-minuscnt=0 #빼기를 기준으로 수식을 나눌 거임
-minuslist=list()
 for i in range(len(a)):
-    if(a[i]=="-"):
-        minuscnt+=1
-        for j range(i):
-            minus.append(a[j])#빼기 앞에 있는 수 저장
-            summinus-=int(minus))#정수로 바꿔서
-            minuslist.append(summinus)
-        minus.clear()
+    if a[i]=="-":
+        eq.append(int(tmp))
+        eq.append("-")
+        tmp=""
+    elif a[i]=="+":
+        eq.append(int(tmp))
+        tmp=""
+    else:
+        tmp+=(a[i])
+eq.append(int(tmp))
+check=list()#마이너스 연산자의 위치를 저장하는 리스트
+for i in range(len(eq)):
+    if eq[i]=="-":
+        check.append(i)
+
+if len(check):#check리스트가 비어있지 않다면
+    first_sum=sum(eq[0:check[0]])#마이너스 연산자가 나오기 전까지 더한 값
+    for i in range(len(check)):
+        if i+1 < len(check):
+            first_sum-=sum(eq[check[i]+1:check[i+1]])
+        else:
+            first_sum-=sum(eq[check[i]+1:])
+    print(first_sum)
+else:#check 리스트가 빈 경우=마이너스 연산자가 없는 식
+    print(sum(eq))
