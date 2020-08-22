0637731bf33d9bf27c6e9a49f24b0f58052b2daf
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-08-23 00:23:28 +0900

    upload file

--- Baekjoon/[2839번] 설탕 배달.py
+++ Baekjoon/[2839번] 설탕 배달.py
@@ -2,21 +2,15 @@
 #백준 2839번
 
 n=int(input())
-a=[5,3]
 cnt=0
-if(n==4):
-    cnt=-1
-elif(n<3):
-    cnt=n-3
-for i in a:
-    tmp=int(n/i)
-    cnt=cnt+tmp
-    n=n%i
-    print(tmp,cnt,n)#몫, 몫을 더한 값, 나머지
-    if(n==0):
+for i in range(1667):
+    for j in range(1001):
+        if(3*i+5*j==n):
+            cnt=i+j
+            break
+    if(cnt!=0):
         break
-    elif((n/3)<1):
-        cnt=cnt+1
-print(cnt)
-
-
+if(cnt==0):
+    print(-1)
+else:
+    print(cnt)
