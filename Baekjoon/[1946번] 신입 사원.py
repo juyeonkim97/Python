41ff050472d71447533175d56360d5b69d05a180
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-08-25 13:39:22 +0900

    update

--- Baekjoon/[1946번] 신입 사원.py
+++ Baekjoon/[1946번] 신입 사원.py
@@ -10,7 +10,7 @@ for i in range(t):
     for j in range(n):
         a,b=map(int,sys.stdin.readline().split())
         alist.append((a,b))
-        alist=sorted(alist,key=lambda alist:alist[0])
+    alist=sorted(alist,key=lambda alist:alist[0])     
     for z in range(1,len(alist)):#1위는 어쨌든 하나라도 이겼으니까 볼 필요 없음
         for j in range(z):#z가 1일때, 순위는 2위 비교대상은 인덱스 0인 1위
             if alist[z][1]>alist[j][1]:
