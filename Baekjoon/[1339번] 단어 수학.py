ff25dd8b5cd105bbfd32160ff0e0741a3a5c77a3
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-08-29 23:49:44 +0900

    update

--- Baekjoon/[1339번] 단어 수학.py
+++ Baekjoon/[1339번] 단어 수학.py
@@ -8,30 +8,24 @@ sum=0
 num=9
 for i in range(n):
     a=list(input())
-    a.reverse()
     strlist.append(a)
-strlist.sort(key=len,reverse=True)#긴 순서대로 정렬
-print(strlist)
-abclist=list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")#abc의 인덱스용
-afabclist=list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")#숫자로 바꾼 후(after abclist)
-abcpointlist=list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")#자릿수대로 가중치별로 계산한 리스
-for j in range(len(strlist[0]),-1,-1):
-    for i in range(n):
-        try:
-            if strlist[i][j] in afabclist:
-                tmp=abclist.index(strlist[i][j])
-                afabclist[tmp]=num
-                num-=1
-        except:
-            continue
-        else:
-            continue
-print(afabclist)
+pointlist=[0]*26
+for i in range(n):#자리별로 가중치 계산
+    tmp=len(strlist[i])
+    for j in range(tmp):
+        pointlist[ord(strlist[i][j])-65]+=1*(tmp-j)
 for i in range(n):
-    mult=1
-    for j in range(len(strlist[i])):
-        tmp=abclist.index(strlist[i][j])#abclist에서 찾고
-        sum=sum+afabclist[tmp]*mult#afabclist 값 대입
-        mult*=10
-print(sum)
-                
+    strlist[i][0]=pointlist[ord(strlist[i][0])-65]
+strlist=sorted(strlist,key=lambda strlist:strlist[0])
+strlist.sort(key=len,reverse=True)
+print(strlist)
+##for i in range(n):
+##    for j in range(len(strlist[i])):
+##        pointlist[ord(strlist[i][j])-65]+=1*(tmp-j)
+##for i in range(n):
+##    mult=1
+##    for j in range(len(strlist[i])):
+##        tmp=abclist.index(strlist[i][j])#abclist에서 찾고
+##        sum=sum+afabclist[tmp]*mult#afabclist 값 대입
+##        mult*=10
+##print(sum)
