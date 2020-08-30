04bc139c14f301ec6357cdce75901ed48ff82d4b
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-08-30 23:28:46 +0900

    update

--- Baekjoon/[1339번] 단어 수학.py
+++ Baekjoon/[1339번] 단어 수학.py
@@ -8,24 +8,19 @@ sum=0
 num=9
 for i in range(n):
     a=list(input())
+    a.reverse()    
     strlist.append(a)
-pointlist=[0]*26
-for i in range(n):#자리별로 가중치 계산
-    tmp=len(strlist[i])
-    for j in range(tmp):
-        pointlist[ord(strlist[i][j])-65]+=1*(tmp-j)
-for i in range(n):
-    strlist[i][0]=pointlist[ord(strlist[i][0])-65]
-strlist=sorted(strlist,key=lambda strlist:strlist[0])
 strlist.sort(key=len,reverse=True)
-print(strlist)
-##for i in range(n):
-##    for j in range(len(strlist[i])):
-##        pointlist[ord(strlist[i][j])-65]+=1*(tmp-j)
-##for i in range(n):
-##    mult=1
-##    for j in range(len(strlist[i])):
-##        tmp=abclist.index(strlist[i][j])#abclist에서 찾고
-##        sum=sum+afabclist[tmp]*mult#afabclist 값 대입
-##        mult*=10
-##print(sum)
+pointlist=[0]*26
+for i in range(n):
+    mult=1
+    for j in range(len(strlist[i])):
+        pointlist[ord(strlist[i][j])-65]+=mult*1
+        mult*=10
+pointlist.sort(reverse=True)
+for i in pointlist:
+    sum+=i*num
+    num-=1
+    if i==0:
+        break
+print(sum)
