328262284bf4ceb49100f8fe312a01d90c214d61
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-08-31 22:44:14 +0900

    upload file

---
+++ Baekjoon/[1080번] 행렬.py
@@ -0,0 +1,18 @@
+#그리디 알고리즘
+#백준 1080번
+
+n,m=map(int,input().split())
+arraya=[[0]*m for i in range(n)]
+arrayb=[[0]*m for i in range(n)]
+cnt=0
+for i in range(n):
+    a=list(input())
+    arraya[i]=a
+print(array)
+for i in range(n):
+    a=list(input())
+    arrayb[i]=a
+for i in range(n):
+    for j in range(m):
+        if arraya[i][j]!=arrayb[i][b]:
+            
