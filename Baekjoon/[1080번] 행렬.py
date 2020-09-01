3a6661ffd49dfc90f8b5d18978fc4f10e7b2f5bc
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-09-01 23:31:38 +0900

    update

--- Baekjoon/[1080번] 행렬.py
+++ Baekjoon/[1080번] 행렬.py
@@ -2,17 +2,25 @@
 #백준 1080번
 
 n,m=map(int,input().split())
-arraya=[[0]*m for i in range(n)]
-arrayb=[[0]*m for i in range(n)]
+A=list()
+B=list()
 cnt=0
 for i in range(n):
-    a=list(input())
-    arraya[i]=a
-print(array)
+    A.append(list(map(int,input())))
 for i in range(n):
-    a=list(input())
-    arrayb[i]=a
-for i in range(n):
-    for j in range(m):
-        if arraya[i][j]!=arrayb[i][b]:
-            
+    B.append(list(map(int,input())))
+for i in range(n-2):
+    for j in range(m-2):
+        if A[i][j]!=B[i][j]:
+            cnt+=1
+            for I in range(i,i+3):
+                for J in range(j,j+3):
+                    if A[I][J]==0:
+                        A[I][J]=1
+                    else:
+                        A[I][J]=0
+
+if A==B:
+    print(cnt)
+else:
+    print(-1)
