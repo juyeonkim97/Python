7a4f2bfe65b3ebda84da55da4629a8d13b593b10
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-09-06 00:01:16 +0900

    update

--- Baekjoon/[1744번] 수 묶기.py
+++ Baekjoon/[1744번] 수 묶기.py
@@ -3,33 +3,52 @@
 
 n=int(input())
 A=list()
+B=list()
+res=list()#결과 리스트
 for i in range(n):
     a=int(input())
-    A.append(a)
-A.sort(reverse=True)
-res=list()#결과 리스트
+    if(a<=0):#0포함 음수 리스트
+        A.append(a) 
+    elif(a==1):#1은 들어올 때부터 플러스 합 리스트에 넣기
+        res.append(1)
+    else:#양수 리스트
+        B.append(a)
+A.sort()
+B.sort(reverse=True)#양수 리스트는 큰 순서대로
 mult=1
 for i in range(len(A)):
-    if(A[i]<0):#음수가 연달아 세개 나올 경우
-        if(A[i-1]<=0):
-            if(mult==1):
-                mult=mult*A[i]
-            else:
-                mult=mult*A[i]
-                res.append(mult)
-                mult=1
-        else:
-            res.append(A[i])
-    elif(A[i]==1):
-        res.append(A[i])
-    elif(A[i]>0 and (i!=n-1 and A[i-1]!=1)):
-        if(mult==1):
-            mult=mult*A[i]
-        else:
-            mult=mult*A[i]
-            res.append(mult)
-            mult=1
+    if(len(A)%2==0):#갯수가 짝수이면 짝 묶어서 계속 곱하기
+       if(mult==1):
+           mult=mult*A[i]
+       else:
+           mult=mult*A[i]
+           res.append(mult)
+           mult=1
+    else:#홀수면 마지막 하나 냅두고 짝 묶어서 계속 곱하기
+       if(mult==1):
+           mult=mult*A[i]
+       else:
+           mult=mult*A[i]
+           res.append(mult)
+           mult=1
+       if(i==len(A)-1):
+           res.append(A[i])
+mult=1
+for i in range(len(B)):
+    if(len(B)%2==0):
+       if(mult==1):
+           mult=mult*B[i]
+       else:
+           mult=mult*B[i]
+           res.append(mult)
+           mult=1
     else:
-        res.append(A[i])
-    print(res)
+       if(mult==1):
+           mult=mult*B[i]
+       else:
+           mult=mult*B[i]
+           res.append(mult)
+           mult=1
+       if(i==len(B)-1):
+           res.append(B[i])     
 print(sum(res))
