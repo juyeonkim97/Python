73faf6613f4c75d698ee098f5e0d39f07449a5fd
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-09-10 00:01:54 +0900

    update

--- Baekjoon/[2437번] 저울.py
+++ Baekjoon/[2437번] 저울.py
@@ -5,22 +5,10 @@ import sys
 n=int(sys.stdin.readline())
 A=list(map(int,sys.stdin.readline().split()))
 A.sort()
-num=1
-con=True
-while(con):
-    tmp=num
-    for i in range(len(A)):
-        if(A[i]>=num):
-            index=i
-            break
-    for i in range(index,-1,-1):
-        if(A[i]<=tmp):
-            tmp=tmp-A[i]
-        if(tmp==0):
-            num+=1
-            break
-        elif(i==0 and tmp!=0):
-            con=False
-            break
-print(num)
-    
+sum=0
+for i in range(len(A)):
+    if sum+1 >= A[i]:
+        sum+=A[i]
+    else:
+        break
+print(sum+1)
