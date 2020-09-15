0532b49ee3af3b539ba7afa7e88840d00b65d4f8
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-09-16 00:46:25 +0900

    upload file

---
+++ K번째 수.py
@@ -0,0 +1,10 @@
+t=int(input())
+b=list()
+for i in range(t):
+    n,s,e,k=map(int,input().split())
+    a=list(map(int,input().split()))
+    b.append()
+print(b)
+for i in range(t):
+    print("#{} {}".format(i+1,b[i][
+
