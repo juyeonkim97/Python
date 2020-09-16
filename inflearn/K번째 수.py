094492e60978deae64ca2e007b284be9bd03660c
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-09-16 23:34:47 +0900

    update

--- inflearn/K번째 수.py
+++ inflearn/K번째 수.py
@@ -1,10 +1,12 @@
 t=int(input())
 b=list()
+klist=list()
 for i in range(t):
     n,s,e,k=map(int,input().split())
     a=list(map(int,input().split()))
-    b.append()
-print(b)
+    a=sorted(a[s-1:e])
+    klist.append(k)
+    b.append(a)
+print(klist)
 for i in range(t):
-    print("#{} {}".format(i+1,b[i][
-
+    print("#{} {}".format(i+1,b[i][klist[i]-1]))
