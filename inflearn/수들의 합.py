3247b7981bf633ca02137e465104d446859e3425
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-09-30 00:05:33 +0900

    인덱스 활용하기

---
+++ inflearn/수들의 합.py
@@ -0,0 +1,21 @@
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+lt=0
+rt=1
+tot=a[lt]#tot는 인덱스 lt와 rt 전 인덱스 사이 값의 합
+cnt=0
+while(True):
+    if(tot<m):
+        if(rt<n):
+            tot+=a[rt]
+            rt+=1
+        else:
+            break
+    elif(tot==m):
+        cnt+=1
+        tot-=a[lt]
+        lt+=1
+    elif(tot>m):
+        tot-=a[lt]
+        lt+=1
+print(cnt)
