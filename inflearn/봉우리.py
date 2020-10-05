4c2e5dbe37d60e6bb7f4f0c9cafbc6d66a3ac136
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-10-05 23:55:18 +0900

    all 함수

--- inflearn/봉우리.py
+++ inflearn/봉우리.py
@@ -1,6 +1,15 @@
 n=int(input())
-a=[list(map(int,input().split())) for _ range(n)]
+a=[list(map(int,input().split())) for _ in range(n)]
+a.insert(0,[0]*n)
+a.append([0]*n)
+for x in a:
+    x.insert(0,0)
+    x.append(0)
+dx=[-1,1,0,0]#상하좌우 움직이는 변수
+dy=[0,0,-1,1]
 cnt=0
-for i in range(n):
-    for j in range(n):
-        if(i in [0,n-1] and j in [0,n-1]
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if all(a[i][j]>a[i+dx[k]][j+dy[k]] for k in range(4)):
+            cnt+=1
+print(cnt)
