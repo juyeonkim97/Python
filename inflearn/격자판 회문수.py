3e1930011b3c5b49712677006ad5fc4c21290a88
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-10-09 00:36:51 +0900

    for-else 문, 리스트 슬라이스 기능 활용하기

--- inflearn/격자판 회문수.py
+++ inflearn/격자판 회문수.py
@@ -1,1 +1,13 @@
 a=[list(map(int,input().split())) for _ in range(7)]
+cnt=0
+for i in range(3):
+    for j in range(7):
+        tmp=a[j][i:i+5]
+        if(tmp==tmp[::-1]):
+            cnt+=1
+        for z in range(2):
+            if(a[i+z][j]!=a[i+4-z][j]):
+                break
+        else: #for문이 break 되지 않았을 때 실행
+            cnt+=1
+print(cnt)
