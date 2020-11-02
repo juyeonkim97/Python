f6aa371d06d0980d63d03339429aefab71b17135
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-11-02 21:34:31 +0900

    upload file

---
+++ inflearn/씨름 선수.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=list()
+for i in range(n):
+    l,w=map(int,input().split())
+    a.append((l,w))
+cnt=0
+a.sort()
+for i in range(n):
+    for j in range(n):
+        
+print(cnt)
+        
