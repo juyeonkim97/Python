bdcdc59f39b6e1cac36f79817444559255038415
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-11-13 22:55:00 +0900

    upload file

--- inflearn/증가 수열.py
+++ inflearn/증가 수열.py
@@ -1,29 +1,25 @@
-from collections import deque
 n=int(input())
-a=list(map(int,input().split()))
-a=deque(a)
-cd=0
-lrstr=""
-for i in range(n-1):
-    if(len(a)==1 and a[0]>cd):
-        lrstr+="L"
-    if(a[0]>cd and a[-1]>cd):
-        if(a[0]< a[-1]):
-            cd=a[0]
-            a.popleft()
-            lrstr+="L"
-        else:
-            cd=a[-1]
-            a.pop()
-            lrstr+="R"
+a=list(map(int, input().split()))
+lt=0
+rt=n-1
+last=0
+res=""
+tmp=[]
+while(lt<=rt):
+    if a[lt]>last:
+        tmp.append((a[lt],'L'))
+    if (a[rt]>last):
+        tmp.append((a[rt],'R'))
+    tmp.sort() #튜플의 첫번째 값으로 정렬
+    if(len(tmp)==0): #리스트에 값이 없다는 건 둘 다 last보다 작음
+        break
     else:
-        if(a[0]>cd):
-            cd=a[0]
-            a.popleft()
-            lrstr+="L"
+        res=res+tmp[0][1] #작은 값에 해당하는 스트링
+        last=tmp[0][0]
+        if(tmp[0][1]=='L'): #작은 값이 왼쪽인 경우
+            lt+=1
         else:
-            cd=a[-1]
-            a.pop()
-            lrstr+="R"
-print(len(lrstr))
-print(lrstr)
+            rt-=1
+    tmp.clear()
+print(len(res))
+print(res)
