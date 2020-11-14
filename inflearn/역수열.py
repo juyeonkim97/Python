6d4464f38bb22796fe1bdf73c7299098d53e0687
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-11-15 00:18:29 +0900

    upload file

--- inflearn/역수열.py
+++ inflearn/역수열.py
@@ -1,3 +1,13 @@
 n=int(input())
 a=list(map(int,input().split()))
 b=[0]*n
+for i in range(n):
+    for j in range(n):
+        if(a[i]== 0 and b[j]==0):
+            b[j]=i+1
+            break
+        elif(b[j]==0):
+            a[i]-=1     
+
+for i in b:
+    print(i,end=" ")
