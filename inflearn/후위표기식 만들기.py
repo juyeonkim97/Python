19a00d8a2b2726da0a6aa482eb230faf9421ffc2
Juyeon Kim <61386041+juyeonkim97@users.noreply.github.com>
2020-11-19 23:04:08 +0900

    stack

--- inflearn/후위표기식 만들기.py
+++ inflearn/후위표기식 만들기.py
@@ -1,33 +1,26 @@
- a=list(str(input()))
-stack=list()
+import sys
+sys.stdin=open("input.txt", "r")
+a=input()
+stack=[]
 res=''
-for i in range(len(a)):
-    if(a[i] in ('0','1','2','3','4','5','6','7','8','9')):
-        res+=a[i]
-        print(res)
-    elif(len(stack)==0):
-        stack.append(a[i])
-    elif(a[i]=='(' or a[i]=='*' or a[i]=='/'):
-        stack.append(a[i])
-        print(stack)
-    elif(a[i]==')'):
-        while(stack):
-            if(stack[-1]=='('):
-                stack.pop()
-                print(stack)
-            else:
+for x in a:
+    if x.isdecimal():
+        res+=x
+    else:
+        if x=='(':
+            stack.append(x)
+        elif x=='*' or x=='/':
+            while stack and (stack[-1]=='*' or stack[-1]=='/'):
                 res+=stack.pop()
-                print(stack)
-    elif(a[i]=='+' or a[i]=='-'):
-        if(stack[-1]=='*' or stack[-1]=='/'):
-            while(stack):
+            stack.append(x)
+        elif x=='+' or x=='-':
+            while stack and stack[-1]!='(':
                 res+=stack.pop()
-            stack.append(a[i])
-            print(stack)
-        else:
-            stack.append(a[i])
-            print(stack)
-            
-while(stack):
+            stack.append(x)
+        elif x==')':
+            while stack and stack[-1]!='(':
+                res+=stack.pop()
+            stack.pop()
+while stack:
     res+=stack.pop()
 print(res)
