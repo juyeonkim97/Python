817797e402c67a9dcfdb5c88949941636d9dbfd3
juyeonkim97 <lhkl@naver.com>
2020-12-26 23:50:07 +0900

    stack, add comment

--- inflearn/쇠막대기.py
+++ inflearn/쇠막대기.py
@@ -2,14 +2,13 @@ a=list(str(input()))
 stack=list()
 res=0
 for i in range(len(a)):
-    if(a[i]=='('):
+    if(a[i]=='('): #여는 괄호면 무조건 append
         stack.append(a[i])
-    elif(a[i]==')'):
-        if(a[i-1]=='('):
+    elif(a[i]==')'): #닫는 괄호면 앞을 확인
+        if(a[i-1]=='('): #앞에 있는 건 a[i-1]
             stack.pop()
-            res+=len(stack)
+            res+=len(stack) 
         else:
             stack.pop()
             res+=1
 print(res)
-            
